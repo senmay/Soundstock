8af4b2b5aa8e06cd31095a87447210218250cb45
dominik <dominik.szarzynski@gmail.com>
2023-09-25 18:16:20 +0200

    first commit

---
+++ src/main/java/com/soundstock/SoundstockApplication.java
@@ -0,0 +1,13 @@
+package com.soundstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundstockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soundstock/SoundstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
